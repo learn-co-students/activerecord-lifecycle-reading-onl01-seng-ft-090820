f36a7acfd966f532f64fdbb37a549200df7a2de1
am20dipi <am20dipi@alum.siena.edu>
2020-11-11 22:21:02 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+  #when u are modifying an attribute of a model, use before_validation -- if you are doing some other action, then use before_save
+
   private
 
   def is_title_case
